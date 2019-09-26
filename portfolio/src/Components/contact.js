80e584a1dc7c75a31be68e81cf07dd79106eb9a2
schwarro <rob.ap.schwartz@gmail.com>
2019-09-26 16:37:02 -0400

    created contact page, needs proper photo

--- portfolio/src/Components/contact.js
+++ portfolio/src/Components/contact.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react'
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl'
 
 class Contact extends Component {
   render() {
     return (
-      <div><h1>Landing Page</h1></div>
+      <div className="contactBody">
+        <Grid className="contactGrid">
+          <Cell col={6}>
+            <h2>Rob Schwartz</h2>
+            <img
+              src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+              alt="avatar"
+              style={{height: '250px'}}
+            />
+            <p style={{width: '75%', margin: 'auto', paddingTop: '1em'}}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </Cell>
+
+
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr/>
+
+            <div className="contactList">
+              <List>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '20px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-phone-square" aria-hidden="true"/> (647) 239-7627
+                  </ListItemContent>
+                </ListItem>
+
+                <ListItem>
+                  <ListItemContent style={{fontSize: '20px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-envelope" aria-hidden="true"/> rob.ap.schwartz@gmail.com
+                  </ListItemContent>
+                </ListItem>
+
+              </List>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
     )
   }
 }
